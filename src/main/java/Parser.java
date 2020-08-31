f35b4da858f68aeeccd4ed60e663517eebd1ca20
theokont <theo.kontaxis@gmail.com>
2020-08-31 23:19:43 +0300

    Added parseCallBackCode method and handled an exception for invalid URLs

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -6,6 +6,28 @@ import java.util.*;
 public class Parser {
 
 
+    public String parseCallBackCode(String link) {
+        StringBuilder code = new StringBuilder();
+        int pointer = 0;
+        for (int i = 0; i < link.length(); i++) {
+            if (link.charAt(i) == '=') {
+                String subStr = link.substring(i-4,i);
+                if (subStr.equals("code")) {
+                    pointer = i;
+                }
+            }
+            if (i > pointer && pointer != 0) {
+                code.append(link.charAt(i));
+            }
+        }
+        if (code.length() == 0) {
+            throw new IllegalStateException("Something went wrong, URI didn't contain a code");
+        }
+        else {
+            return String.valueOf(code);
+        }
+    }
+
     public String parseListID(String URL) {
         StringBuilder listID = new StringBuilder();
         int pointer = 0;
@@ -22,7 +44,12 @@ public class Parser {
                 idLength++;
             }
         }
-        return String.valueOf(listID);
+        if (listID.length() == 0) {
+            throw new IllegalStateException("Something went wrong, URL didn't contain a list ID");
+        }
+        else {
+            return String.valueOf(listID);
+        }
     }
 
     public ArrayList<String> readJsonResponse(String jsonResponse) throws IOException {
